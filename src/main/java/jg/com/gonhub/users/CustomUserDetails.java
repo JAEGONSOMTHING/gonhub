11dedb8d301f2cd8d58e832f0e51db7666918aee
IM JAE GON <89837407+jaegonSex@users.noreply.github.com>
2022-04-22 15:41:15 +0900

    login method using jwt token

---
+++ src/main/java/jg/com/gonhub/users/CustomUserDetails.java
@@ -0,0 +1,56 @@
+package jg.com.gonhub.users;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Getter
+public class CustomUserDetails implements UserDetails {
+    private final User account;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        String authority = account.getRole().getAuthority();
+        List<GrantedAuthority> role = new ArrayList<>();
+        role.add(new SimpleGrantedAuthority(authority));
+        return role;
+    }
+
+    @Override
+    public String getPassword() {
+        return account.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return account.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/jg/com/gonhub/users/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package jg.com.gonhub.users;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("존재하지 않는 사용자입니다"));
+
+        UserDetails userDetails = new CustomUserDetails(user);
+        return userDetails;
+    }
+}
